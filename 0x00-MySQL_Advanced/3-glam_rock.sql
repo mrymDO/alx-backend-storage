11ee062c65393a0ecb552bbe3000662d6b55031e
Dobli <dobli.meryeme@gmail.com>
2023-12-14 20:43:40 +0000

    fix task3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,6 @@
 -- List bands with Glam rock as their main style, ranked by longevity
 
-
 SELECT
-   band_name,
-   (IFNULL(split, '2020') - formed) AS lifespan
-FROM
-   metal_bands
-WHERE
-   style LIKE '%Glam rock%';
+    band_name, (IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%';
