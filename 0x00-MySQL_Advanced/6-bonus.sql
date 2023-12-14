d29cea59741c42c57e08ca475a2ff1d484fb7b18
Dobli <dobli.meryeme@gmail.com>
2023-12-14 08:27:39 +0000

    task6 fix

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -13,23 +13,18 @@ BEGIN
 	DECLARE project_id INT DEFAULT 0;
 	DECLARE project_count INT DEFAULT 0;
 
-        -- Check if the project already exists
-        SELECT id INTO project_id
-        FROM projects
-        WHERE name = project_name;
-
         -- If the project does not exist, create it
-        SELECT COUNT(id) INTO project_count FROM projects
+	SELECT COUNT(*) INTO project_count FROM projects
 	WHERE name = project_name;
 	IF project_count = 0 THEN
-            INSERT INTO projects (name) VALUES (project_name);
-            SET project_id = LAST_INSERT_ID();
+		INSERT INTO projects (name) VALUES (project_name);
         END IF;
+	
+	SELECT id INTO project_id
+	FROM projects
+	WHERE name = project_name;
 
-	SELECT user_id, project_id, score;
-	SELECT project_name, project_id;
-
-	 INSERT INTO corrections (user_id, project_id, score)
+	INSERT INTO corrections (user_id, project_id, score)
         VALUES (
                 user_id,
                 project_id,
