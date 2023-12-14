ff0dc8bece18649d65fe86ad462c920d1868793f
Dobli <dobli.meryeme@gmail.com>
2023-12-14 09:41:26 +0000

    fix task10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,14 +1,14 @@
 -- creates a function SafeDiv that divides (and returns) the first by the second number
 -- or returns 0 if the second number is equal to 0.
 
-DROP FUNCTION IF IT EXISTS SafeDiv;
+DROP FUNCTION IF EXISTS SafeDiv;
 
 DELIMITER $$
 
-CREATE DUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 BEGIN
 	DECLARE result FLOAT DEFAULT 0;
-	IF b == 0 THEN
+	IF b = 0 THEN
 		RETURN 0;
 	ELSE
 		RETURN a / b;
