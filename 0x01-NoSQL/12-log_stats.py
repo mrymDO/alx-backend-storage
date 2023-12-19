bb2cf3b518b969e03f271c8ede867be2b4cd164b
Mrym <dobli.meryeme@gmail.com>
2023-12-19 03:14:38 -0800

    task12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,28 +5,28 @@ from pymongo import MongoClient
 
 
 
-def nginx_logs_stats():
+def nginx_logs_stats(mongo_collection):
     """stats about Nginx logs"""
-    client = MongoClient('mongodb://localhost:27017')
-    db = client.logs
-    nginx_collection = db.nginx
 
-    total_logs = nginx_collection.count_documents({})
+    total_logs = mongo_collection.count_documents({})
 
     print(f"{total_logs} logs")
 
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = nginx_collection.count_documents({"method": method})
+        count = mongo_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
 
 
-    count_status_requests = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    count_status_requests = mongo_collection.count_documents({"method": "GET", "path": "/status"})
     print(f"\t{count_status_requests} status check")
 
     client.close()
 
 if __name__ == "__main__":
-    nginx_logs_stats()
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+    nginx_logs_stats(collection)
