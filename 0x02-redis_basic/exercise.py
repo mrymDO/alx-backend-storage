0c7ad86a6c02db2cc332d46c73b36c8da8194a31
mrym <dobli.meryeme@gmail.com>
2023-12-20 15:42:30 +0000

    task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -20,3 +20,32 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """retrieve data from the cache based on a key"""
+        data = self._redis.get(key)
+
+        if data and fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> int:
+        """retrieve data as a string from the cache"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """retrieve data as an integer from the cache"""
+        return self.get(key, int)
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
